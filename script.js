6b2a0778590a18feb67f465c3725b5a6efd46fc2
Aiswarya_Purushothaman <133517332+aiswaryachand@users.noreply.github.com>
2023-10-12 23:33:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function foo()
+{
+        var res =document.getElementById("firstname").value;
+        console.log(res);
+        var res =document.getElementById("middlename").value;
+        console.log(res);
+        var res =document.getElementById("lastname").value;
+        console.log(res);
+        var res =document.getElementById("email").value;
+        console.log(res);
+        var res =document.getElementById("Pincode").value;
+        console.log(res);
+}
